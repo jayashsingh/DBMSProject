27cd4e9a98c4a4c0b41183e50552311497fdcc19
Tahaa17 <52828481+Tahaa17@users.noreply.github.com>
2020-11-16 13:39:26 -0500

    More user creation

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const User = require("../models/userModel.js");
-const alert = require("js-alert");
+const pop = require('node-popup');
+const sql = require("../models/db.js")
 // Create and Save a new Customer
 exports.create = (req, res) => {
   // Validate request
@@ -9,25 +10,37 @@ exports.create = (req, res) => {
     });
   }
   console.log(req.body.UsernameRegister);
-  // Create a Customer
-  const user = new User({
-    Username: req.body.UsernameRegister,
-    Password: req.body.PasswordRegister
+  //Check if user exists already
+  sql.query("SELECT Username FROM LoginInfo WHERE Username = '"+req.body.UsernameRegister+"'",function(err,res1,field){
+    console.log("lengthazz" + res1.length);
+    if(res1.length>0)
+    {
+      pop.alert('User with that userame exists already!');
+      //res.redirect('/register.html');
+    }
+    else {
+      // Create a Customer
+      const user = new User({
+        Username: req.body.UsernameRegister,
+        Password: req.body.PasswordRegister
+      });
+
+      // Save Customer in the database
+      User.create(user, (err, data) => {
+        if (err)
+          res.status(500).send({
+            message:
+              err.message || "Some error occurred while creating the User."
+          });
+        else{
+        //res.send(data);
+        console.log("Data was stored");
+        alert.alert("Registreed");
+        //popup.alert({content:'Registration successful! You may now login!'});
+        res.redirect('/');
+      }
+      });
+    }
   });
 
-  // Save Customer in the database
-  User.create(user, (err, data) => {
-    if (err)
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while creating the User."
-      });
-    else{
-    //res.send(data);
-    console.log("Data was stored");
-    alert.alert("Registreed");
-    //popup.alert({content:'Registration successful! You may now login!'});
-    res.redirect('/');
-  }
-  });
 };
