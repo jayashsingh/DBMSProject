95bf7b4f70293c4c085475f0265b2c8bddb0e720
Jayash <30718465+jayashsingh@users.noreply.github.com>
2021-02-23 21:01:54 -0500

    Delete db.config.js

--- Config/db.config.js
+++
@@ -1,7 +0,0 @@
-//Config file holding connection details
-module.exports = {
-  HOST: "dbms-project.cvwl50l2zoqx.ca-central-1.rds.amazonaws.com",
-  USER: "admin",
-  PASSWORD: "5pHa2JN7KfNXZ4WH",
-  DB: "Job_Site"
-};
